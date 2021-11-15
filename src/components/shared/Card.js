8cdf2947eff1020f96ee389af6a21b49df722323
unknown <zil.kupcin@gmail.com>
2021-11-15 18:25:12 +0200

    Create a Card styled component

---
+++ src/components/shared/Card.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+    background-color: white;
+    padding: 24px;
+    border-radius: 20px;
+    align-self: stretch;
+`
+
+export default Card;
