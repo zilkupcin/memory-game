8cbcf751325fae9e4995ccabe4946b83ab9e4268
unknown <zil.kupcin@gmail.com>
2021-11-20 21:23:26 +0200

    Change ResultsModal style

--- src/components/ResultsModal.js
+++ src/components/ResultsModal.js
@@ -10,6 +10,7 @@ const Container = styled.div`
     bottom: 0;
     right: 0;
     left: 0;
+    z-index: 10;
     
     &::before {
         position: absolute;
@@ -27,6 +28,8 @@ const Modal = styled.div`
     padding: 32px;
     border-radius: 20px;
     text-align: center;
+    width: 100%;
+    margin: 0 24px;
 `;
 
 const Title = styled.h2`
@@ -49,18 +52,19 @@ const ResultsList = styled.ul`
 const Result = styled.li`
     display: flex;
     justify-content: space-between;
-    background-color: ${props => props.theme.colors.mystic};
+    background-color: ${props => props.isWinner ? props.theme.colors.pickledBluewood : props.theme.colors.mystic};
     border-radius: 6px;
     padding: 16px;
     margin-bottom: 12px;
 
     span:first-child {
-        color: ${props => props.theme.colors.gothic}
+        color: ${props => props.isWinner ? "white" : props.theme.colors.gothic};
+        font-size: 18px;
     }
 
-    span:second-child {
-        font-size: 24px;
-        color: ${props => props.theme.colors.pickledBluewood}
+    span:last-child {
+        font-size: 20px;
+        color: ${props => props.isWinner ? "white" : props.theme.colors.pickledBluewood};
     }
 
     :last-child {
@@ -69,39 +73,39 @@ const Result = styled.li`
 `
 
 const ButtonGroup = styled.div`
-    display: flex;
-
-    * {
-        flex: 1;
-    }
+    display: grid;
+    column-gap: 8px;
+    grid-template-columns: repeat(${props => props.children.length},1fr);
 `;
 
 const sortByScore = (a,b) => {
-    if (a.score > b.score) return 1;
-    if (a.score < b.score) return -1;
+    if (a.score < b.score) return 1;
+    if (a.score > b.score) return -1;
 
     return 0;
 }
 
-const ResultsModal = ({game}) => {
+const ResultsModal = ({game, onRestart, onNewGame}) => {
+    const sortedPlayers = game.players.sort(sortByScore);
+
     return (
         <Container>
             <Modal>
-                <Title>Player 3 Wins!</Title>
+                <Title>{sortedPlayers[0].name} Wins!</Title>
                 <Subtitle>Game over... here are the results:</Subtitle>
                 <ResultsList>
-                    {game.players.sort(sortByScore).map((player,index) => {
+                    {sortedPlayers.map((player,index) => {
                         return (
-                            <Result>
-                                <span>{`Player ${index + 1}`}</span>
+                            <Result isWinner={index === 0}>
+                                <span>{player.name}</span>
                                 <span>{`${player.score} Pairs`}</span>
                             </Result>
                         )
                     })}
                 </ResultsList>
                 <ButtonGroup>
-                    <Button type="primary">Restart</Button>
-                    <Button type="secondary">Setup New Game</Button>
+                    <Button onClick={onRestart} type="primary">Restart</Button>
+                    <Button onClick={onNewGame} type="secondary">Setup New Game</Button>
                 </ButtonGroup>
             </Modal>
         </Container>
